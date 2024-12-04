459dd16ea6ae608af4c80da2d4054e60b3a3d1e4
jonathan0145 <jonathan_bermeo014@hotmail.com>
2024-12-04 16:04:25 -0500

    loading

---
+++ InmotechFrontend/wwwroot/javascript/loading.js
@@ -0,0 +1,13 @@
+﻿const progressBar = document.querySelector('.progress-bar');
+let progress = 0;
+
+function updateProgressBar() {
+    progress += 10;
+    progressBar.style.width = progress + '%';
+
+    if (progress >= 100) {
+        clearInterval(interval);
+    }
+}
+
+const interval = setInterval(updateProgressBar, 500);
